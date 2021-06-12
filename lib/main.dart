1bbd80ea3a1113611ae32ba6a8c4203bc0086aa0
Nipun VV <nipunvv@gmail.com>
2021-06-13 00:10:17 +0530

    Add switch to toggle popular and latest movies

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
 import 'package:movie_bank/constants/constants.dart';
 import 'package:movie_bank/models/Movie.dart';
 import 'package:http/http.dart' as http;
@@ -35,8 +36,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late Future<List<Movie>> futureMovie;
 
-  Future<List<Movie>> fetchMovies() async {
-    String url = "${TMDB_API_URL}movie/popular?language=en-US&page=1";
+  Future<List<Movie>> fetchMovies(int index) async {
+    String url =
+        "${TMDB_API_URL}movie/${index == 0 ? 'popular' : 'now_playing'}?language=en-US&page=1";
     final response = await http.get(
       Uri.parse(url),
       headers: {HttpHeaders.authorizationHeader: "Bearer $TMDB_API_KEY"},
@@ -47,6 +49,8 @@ class _HomeScreenState extends State<HomeScreen> {
       for (Map<String, dynamic> movie in jsonDecode(response.body)['results']) {
         movies.add(Movie.fromJson(movie));
       }
+      if (index == 1) print('MOVIES ==> $movies');
+
       return movies;
     } else {
       throw Exception('Failed to load Movie');
@@ -56,7 +60,13 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    futureMovie = fetchMovies();
+    futureMovie = fetchMovies(0);
+  }
+
+  void handleCategoryChange(int index) {
+    setState(() {
+      futureMovie = fetchMovies(index);
+    });
   }
 
   int getItemCount(double screenWidth) {
@@ -78,40 +88,71 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Movie App'),
       ),
       body: Container(
-        child: FutureBuilder<List<Movie>>(
-          future: futureMovie,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<Movie>? movies = snapshot.data;
-              return GridView.count(
-                crossAxisCount: getItemCount(screenWidth),
-                mainAxisSpacing: 0,
-                shrinkWrap: true,
-                children: List.generate(movies!.length, (index) {
-                  return Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    child: Card(
-                      child: GridTile(
-                        child: Image.network(
-                          "$TMDB_WEB_URL${movies[index].posterPath}",
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-              );
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-
-            return SizedBox(
-              height: 100,
-              width: 100,
-              child: Center(
-                child: CircularProgressIndicator(),
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            children: [
+              FlutterToggleTab(
+                width: 20,
+                borderRadius: 20,
+                height: 30,
+                initialIndex: 0,
+                selectedBackgroundColors: [Colors.blue],
+                selectedTextStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                ),
+                unSelectedTextStyle: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+                labels: ["Popular", "Latest"],
+                selectedLabelIndex: (index) {
+                  handleCategoryChange(index);
+                },
               ),
-            );
-          },
+              Expanded(
+                child: FutureBuilder<List<Movie>>(
+                  future: futureMovie,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      List<Movie>? movies = snapshot.data;
+                      return GridView.count(
+                        crossAxisCount: getItemCount(screenWidth),
+                        mainAxisSpacing: 0,
+                        shrinkWrap: true,
+                        children: List.generate(movies!.length, (index) {
+                          return Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            child: Card(
+                              child: GridTile(
+                                child: Image.network(
+                                  "$TMDB_WEB_URL${movies[index].posterPath}",
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text("${snapshot.error}");
+                    }
+
+                    return SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
