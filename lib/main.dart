c6d0ba93778b653a5452eae3bb7eb345f300b214
Nipun VV <nipunvv@gmail.com>
2021-06-13 22:44:52 +0530

    Setup home page with cover image and customized app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
 import 'package:movie_bank/constants/constants.dart';
 import 'package:movie_bank/models/Movie.dart';
 import 'package:http/http.dart' as http;
+import 'package:movie_bank/widgets/top_bar_contents.dart';
+import 'package:movie_bank/widgets/web_scrollbar.dart';
 
 void main() {
   runApp(MovieBank());
@@ -34,7 +37,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late Future<List<Movie>> futureMovie;
+  late Future<List<Movie>> popularMovies;
+  late Future<List<Movie>> latestMovies;
+  late ScrollController _scrollController;
+  double _scrollPosition = 0;
+  double _opacity = 0;
 
   Future<List<Movie>> fetchMovies(int index) async {
     String url =
@@ -49,7 +56,6 @@ class _HomeScreenState extends State<HomeScreen> {
       for (Map<String, dynamic> movie in jsonDecode(response.body)['results']) {
         movies.add(Movie.fromJson(movie));
       }
-      if (index == 1) print('MOVIES ==> $movies');
 
       return movies;
     } else {
@@ -57,101 +63,144 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  _scrollListener() {
+    setState(() {
+      _scrollPosition = _scrollController.position.pixels;
+    });
+  }
+
   @override
   void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
     super.initState();
-    futureMovie = fetchMovies(0);
+    popularMovies = fetchMovies(0);
+    latestMovies = fetchMovies(1);
   }
 
   void handleCategoryChange(int index) {
     setState(() {
-      futureMovie = fetchMovies(index);
+      popularMovies = fetchMovies(index);
     });
-  }
-
-  int getItemCount(double screenWidth) {
-    if (screenWidth > 1000) {
-      return 5;
-    } else if (screenWidth > 700) {
-      return 4;
-    } else {
-      return 3;
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
+    Size screenSize = MediaQuery.of(context).size;
+    _opacity = _scrollPosition < screenSize.height * 0.40
+        ? _scrollPosition / (screenSize.height * 0.40)
+        : 1;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie App'),
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 1000),
+        child: TopBarContents(_opacity),
       ),
-      body: Container(
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 20),
-          child: Column(
-            children: [
-              FlutterToggleTab(
-                width: 20,
-                borderRadius: 20,
-                height: 30,
-                initialIndex: 0,
-                selectedBackgroundColors: [Colors.blue],
-                selectedTextStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w700,
-                ),
-                unSelectedTextStyle: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
-                labels: ["Popular", "Latest"],
-                selectedLabelIndex: (index) {
-                  handleCategoryChange(index);
-                },
-              ),
-              Expanded(
-                child: FutureBuilder<List<Movie>>(
-                  future: futureMovie,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      List<Movie>? movies = snapshot.data;
-                      return GridView.count(
-                        crossAxisCount: getItemCount(screenWidth),
-                        mainAxisSpacing: 0,
-                        shrinkWrap: true,
-                        children: List.generate(movies!.length, (index) {
-                          return Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 10),
-                            child: Card(
-                              child: GridTile(
-                                child: Image.network(
-                                  "$TMDB_WEB_URL${movies[index].posterPath}",
-                                ),
-                              ),
-                            ),
-                          );
-                        }),
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    }
-
-                    return SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: Center(
-                        child: CircularProgressIndicator(),
+      body: WebScrollbar(
+        color: Colors.blueGrey,
+        backgroundColor: Colors.blueGrey.withOpacity(0.3),
+        width: 10,
+        heightFraction: 0.3,
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          physics: ClampingScrollPhysics(),
+          child: Container(
+            child: Container(
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        child: SizedBox(
+                          height: screenSize.height * 0.5,
+                          width: screenSize.width,
+                          child: Image.asset(
+                            'assets/images/cover.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    );
-                  },
-                ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  FlutterToggleTab(
+                    width: 20,
+                    borderRadius: 20,
+                    height: 30,
+                    initialIndex: 0,
+                    selectedBackgroundColors: [Colors.blue],
+                    selectedTextStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    unSelectedTextStyle: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    labels: ["Popular", "Latest"],
+                    selectedLabelIndex: (index) {
+                      handleCategoryChange(index);
+                    },
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  FutureBuilder<List<Movie>>(
+                    future: popularMovies,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<Movie>? movies = snapshot.data;
+                        return CarouselSlider(
+                          options: CarouselOptions(
+                            height: MediaQuery.of(context).size.height * 0.4,
+                            viewportFraction: 0.2,
+                            enlargeCenterPage: true,
+                            enlargeStrategy: CenterPageEnlargeStrategy.scale,
+                            disableCenter: true,
+                            aspectRatio: 2.0,
+                            initialPage: 0,
+                          ),
+                          items: movies!.map((item) {
+                            return Builder(
+                              builder: (BuildContext context) {
+                                return Container(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 20,
+                                    vertical: 10,
+                                  ),
+                                  child: Image.network(
+                                    "$TMDB_WEB_URL${item.posterPath}",
+                                  ),
+                                );
+                              },
+                            );
+                          }).toList(),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
+
+                      return SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    },
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/top_bar_contents.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class TopBarContents extends StatefulWidget {
+  final double opacity;
+
+  TopBarContents(this.opacity);
+
+  @override
+  _TopBarContentsState createState() => _TopBarContentsState();
+}
+
+class _TopBarContentsState extends State<TopBarContents> {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
+    return PreferredSize(
+      preferredSize: Size(screenSize.width, 1000),
+      child: Container(
+        color: Colors.blueGrey.withOpacity(widget.opacity),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'MOVIE BANK',
+                style: TextStyle(
+                  color: Colors.blueGrey[100],
+                  fontSize: 20,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 3,
+                ),
+              ),
+              SizedBox(
+                width: screenSize.width / 50,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
