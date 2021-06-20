8ff999885def0e67678718afa876d6fcd71ab658
Nipun VV <nipunvv@gmail.com>
2021-06-21 01:33:07 +0530

    Show genres in movie details page

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:movie_bank/constants/constants.dart';
 import 'package:movie_bank/models/Movie.dart';
+import 'package:movie_bank/models/genre.dart';
 import 'package:movie_bank/providers/provider.dart';
 import 'package:movie_bank/widgets/footer.dart';
 import 'package:movie_bank/widgets/top_bar_contents.dart';
@@ -26,6 +27,55 @@ class _MovieDetailState extends State<MovieDetail> {
     setState(() {
       movie = widget.movie;
     });
+  }
+
+  List<Color> colors = [
+    Colors.amber,
+    Colors.blue.shade400,
+    Colors.red.shade400,
+    Colors.orange.shade400,
+  ];
+
+  Widget getGenres(List<Genre> genres) {
+    if (!(movie.genreIds is List)) return Container();
+    List<dynamic> genreIds = movie.genreIds;
+    List<Genre> genreNames = [];
+    genreIds.forEach((id) {
+      genreNames =
+          genres.where((element) => genreIds.contains(element.id)).toList();
+    });
+
+    if (genreNames.length > 0) {
+      return Row(children: [
+        for (var i = 0; i < genreNames.length; i++)
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 3,
+            ),
+            margin: EdgeInsets.only(
+              right: 5,
+            ),
+            decoration: BoxDecoration(
+              color: colors[i % 4],
+              border: Border.all(
+                color: colors[i % 4],
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Text(
+              genreNames[i].name,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Montserrat',
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+      ]);
+    }
+    return Container();
   }
 
   @override
@@ -137,11 +187,11 @@ class _MovieDetailState extends State<MovieDetail> {
                           SizedBox(
                             height: 20,
                           ),
-                          genreModel.genres.length != 0
-                              ? Container(
-                                  child: Text('Genres Loaded'),
-                                )
-                              : Text('Not Loaded'),
+                          if (genreModel.genres.length != 0)
+                            getGenres(genreModel.genres),
+                          SizedBox(
+                            height: 20,
+                          ),
                           Text(
                             'OVERVIEW',
                             style: TextStyle(
