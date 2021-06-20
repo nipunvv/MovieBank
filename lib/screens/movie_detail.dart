4cfae7b672cf3c91a270e0a0d3ba0f02525c7121
Nipun VV <nipunvv@gmail.com>
2021-06-20 22:22:16 +0530

    Change movie details screen design

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:movie_bank/constants/constants.dart';
@@ -36,62 +37,132 @@ class _MovieDetailState extends State<MovieDetail> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          color: Colors.indigo[800],
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Image.network(
-                      "$TMDB_WEB_URL${movie!.backDropPath}",
-                      fit: BoxFit.cover,
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      height: MediaQuery.of(context).size.height * 0.8,
-                      color: const Color.fromRGBO(255, 255, 255, 0.5),
-                      colorBlendMode: BlendMode.modulate,
+              Container(
+                margin: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(
+                          "$TMDB_WEB_URL${movie.posterPath}",
+                          fit: BoxFit.cover,
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          height: MediaQuery.of(context).size.height * 0.8,
+                        ),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 30,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        movie.title,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Montserrat',
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        movie.language,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Montserrat',
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.3,
-                        child: Text(
-                          movie.overview,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Quicksand',
-                            fontSize: 12,
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.8,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              movie.title.toUpperCase(),
+                              style: TextStyle(
+                                color: Colors.black,
+                                // fontFamily: 'Montserrat',
+                                fontSize: 26,
+                                // fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                        ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                  vertical: 5,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Color(0xffd3d3d3),
+                                  border: Border.all(
+                                    color: Color(0xffd3d3d3),
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Text(
+                                  movie.language,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                  vertical: 5,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Color(0xffd3d3d3),
+                                  border: Border.all(
+                                    color: Color(0xffd3d3d3),
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Text(
+                                  movie.releaseDate,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'OVERVIEW',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Quicksand',
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.4,
+                            child: Text(
+                              movie.overview,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'Quicksand',
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 height: 200,
