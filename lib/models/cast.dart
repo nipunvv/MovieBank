ddb0e928633cbcfa873169282f6cda229b02183c
Nipun VV <nipunvv@gmail.com>
2021-07-17 23:35:10 +0530

    Add credit id to cast model

--- lib/models/cast.dart
+++ lib/models/cast.dart
@@ -2,11 +2,13 @@ class Cast {
   final int id;
   final String name;
   final String avatar;
+  final String creditId;
 
   Cast({
     required this.id,
     required this.name,
     required this.avatar,
+    required this.creditId,
   });
 
   factory Cast.fromJson(Map<String, dynamic> json) {
@@ -14,6 +16,7 @@ class Cast {
       id: json['id'],
       name: json['name'],
       avatar: json['profile_path'] ?? '',
+      creditId: json['credit_id'],
     );
   }
 
